9486f5959f88465a1be5d6ab7b38b39598c43c2d
bluetree7878 <bluetree7878@naver.com>
2025-03-29 10:35:41 +0900

    대시보드 페이지 및 레이아웃 추가, VSCode 설정 및 Yarn SDK 구성 파일 생성

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        루트 레이아웃 {children}
       </body>
     </html>
   );
