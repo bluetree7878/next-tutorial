0cf510a59db2629080c204fdf9afa0861d77e4fc
bluetree7878 <bluetree7878@naver.com>
2025-03-30 11:35:42 +0900

    에러 처리 컴포넌트 추가 및 404 페이지 구현, API 응답에서 에러 발생 테스트

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -6,6 +6,8 @@ export async function GET(_request: NextRequest) {
   const randomDelay = Math.floor(Math.random() * 5000);
   console.log("randomDelay", randomDelay);
 
+  throw new Error("에러 발생!");
+
   await delay(randomDelay);
 
   return NextResponse.json({ data: `${randomDelay}만에 응답 완료!` });
